5d87099096a2537f8f1b5f342d808994674544cb
bayram <bay00@outlook.de>
2020-03-07 19:19:43 +0100

    Updates

---
+++ precache-manifest.c365b84ec339a66134904a6ae8b26dcf.js
@@ -0,0 +1,266 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d2a248657fa62d6255a42ca21b9d34f",
+    "url": "/steam-artwork-gen/index.html"
+  },
+  {
+    "revision": "d77d181f27342f7b8dda",
+    "url": "/steam-artwork-gen/static/css/main.e954d683.chunk.css"
+  },
+  {
+    "revision": "e14695fd81517217064c",
+    "url": "/steam-artwork-gen/static/js/2.5e663ef9.chunk.js"
+  },
+  {
+    "revision": "ac9fac6c8e755a8efecd01981adb88e6",
+    "url": "/steam-artwork-gen/static/js/2.5e663ef9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d77d181f27342f7b8dda",
+    "url": "/steam-artwork-gen/static/js/main.3fe1ce69.chunk.js"
+  },
+  {
+    "revision": "0e79bb8bd6258ffb25c4",
+    "url": "/steam-artwork-gen/static/js/runtime-main.33cf8050.js"
+  },
+  {
+    "revision": "051e4d91dc88d10f19090107193629b8",
+    "url": "/steam-artwork-gen/static/media/SFProDisplay-Black.051e4d91.woff2"
+  },
+  {
+    "revision": "84c9de355ee1a96b62924e0e8863e547",
+    "url": "/steam-artwork-gen/static/media/SFProDisplay-Black.84c9de35.woff"
+  },
+  {
+    "revision": "0d5d7dd6282b951a449b350e815f41e1",
+    "url": "/steam-artwork-gen/static/media/SFProDisplay-BlackItalic.0d5d7dd6.woff"
+  },
+  {
+    "revision": "118eb0dde7e80bbeb124c5977e567516",
+    "url": "/steam-artwork-gen/static/media/SFProDisplay-BlackItalic.118eb0dd.woff2"
+  },
+  {
+    "revision": "33802914271ef3d489d31399a5c8c3af",
+    "url": "/steam-artwork-gen/static/media/SFProDisplay-Bold.33802914.woff2"
+  },
+  {
+    "revision": "33dc917d0fee13672178b0115bf59cb1",
+    "url": "/steam-artwork-gen/static/media/SFProDisplay-Bold.33dc917d.woff"
+  },
+  {
+    "revision": "2edb52e3bb9d27109c45fa6de0c3f4ed",
+    "url": "/steam-artwork-gen/static/media/SFProDisplay-BoldItalic.2edb52e3.woff2"
+  },
+  {
+    "revision": "35ef70d51439e0647596c9cd31a41994",
+    "url": "/steam-artwork-gen/static/media/SFProDisplay-BoldItalic.35ef70d5.woff"
+  },
+  {
+    "revision": "b99320da5f1acc3045b58d2a07d87ad5",
+    "url": "/steam-artwork-gen/static/media/SFProDisplay-Heavy.b99320da.woff"
+  },
+  {
+    "revision": "fee26660875bec06ce508e96057f6efa",
+    "url": "/steam-artwork-gen/static/media/SFProDisplay-Heavy.fee26660.woff2"
+  },
+  {
+    "revision": "81fa03533aa615318cb7cdea5c721ce1",
+    "url": "/steam-artwork-gen/static/media/SFProDisplay-HeavyItalic.81fa0353.woff2"
+  },
+  {
+    "revision": "bd6fb35ff30dbdd105329744f7c3bd4c",
+    "url": "/steam-artwork-gen/static/media/SFProDisplay-HeavyItalic.bd6fb35f.woff"
+  },
+  {
+    "revision": "0229ea633558c4c9c1fbc1c92e5a0de3",
+    "url": "/steam-artwork-gen/static/media/SFProDisplay-Light.0229ea63.woff2"
+  },
+  {
+    "revision": "ee7481b4e87fdad06d71aeac1d173821",
+    "url": "/steam-artwork-gen/static/media/SFProDisplay-Light.ee7481b4.woff"
+  },
+  {
+    "revision": "24eb4d29f898a04a9ac369852d56cf9a",
+    "url": "/steam-artwork-gen/static/media/SFProDisplay-LightItalic.24eb4d29.woff2"
+  },
+  {
+    "revision": "6ca51b0b2f9bc4aaaa8d4ea34868e066",
+    "url": "/steam-artwork-gen/static/media/SFProDisplay-LightItalic.6ca51b0b.woff"
+  },
+  {
+    "revision": "52ea0d7008516c101595f81d37225889",
+    "url": "/steam-artwork-gen/static/media/SFProDisplay-Medium.52ea0d70.woff2"
+  },
+  {
+    "revision": "9f7769f51fe242081d7fec9dff7bea58",
+    "url": "/steam-artwork-gen/static/media/SFProDisplay-Medium.9f7769f5.woff"
+  },
+  {
+    "revision": "8047adc9db8258b74263efebcf469657",
+    "url": "/steam-artwork-gen/static/media/SFProDisplay-MediumItalic.8047adc9.woff2"
+  },
+  {
+    "revision": "a9ef96f702f18e5d31598d38e3f1b861",
+    "url": "/steam-artwork-gen/static/media/SFProDisplay-MediumItalic.a9ef96f7.woff"
+  },
+  {
+    "revision": "89a0dda9a4e0fcbf9d90f507feb77486",
+    "url": "/steam-artwork-gen/static/media/SFProDisplay-Regular.89a0dda9.woff"
+  },
+  {
+    "revision": "abe60269bd99b7f36cd026bd02af92a6",
+    "url": "/steam-artwork-gen/static/media/SFProDisplay-Regular.abe60269.woff2"
+  },
+  {
+    "revision": "2d0b455181974a1c3d8e3177e28afa00",
+    "url": "/steam-artwork-gen/static/media/SFProDisplay-RegularItalic.2d0b4551.woff2"
+  },
+  {
+    "revision": "87f09cbb5bc6c7593db4c244ad9305ec",
+    "url": "/steam-artwork-gen/static/media/SFProDisplay-RegularItalic.87f09cbb.woff"
+  },
+  {
+    "revision": "2c151db57c2192e8e7a26ea88d40fa46",
+    "url": "/steam-artwork-gen/static/media/SFProDisplay-Semibold.2c151db5.woff"
+  },
+  {
+    "revision": "af64ab08547cc18daa32439d1f6c9c05",
+    "url": "/steam-artwork-gen/static/media/SFProDisplay-Semibold.af64ab08.woff2"
+  },
+  {
+    "revision": "170a61afa3416357dced39047ece034e",
+    "url": "/steam-artwork-gen/static/media/SFProDisplay-SemiboldItalic.170a61af.woff2"
+  },
+  {
+    "revision": "6143c1a5f0e5ff85b1b8ca13ae27d834",
+    "url": "/steam-artwork-gen/static/media/SFProDisplay-SemiboldItalic.6143c1a5.woff"
+  },
+  {
+    "revision": "03404df387b5f79f56fe6e29682790a6",
+    "url": "/steam-artwork-gen/static/media/SFProDisplay-Thin.03404df3.woff"
+  },
+  {
+    "revision": "16e844ac92f1030c7acd7e5ede40789c",
+    "url": "/steam-artwork-gen/static/media/SFProDisplay-Thin.16e844ac.woff2"
+  },
+  {
+    "revision": "be77a4c97a6b8e565340112fea75288b",
+    "url": "/steam-artwork-gen/static/media/SFProDisplay-ThinItalic.be77a4c9.woff"
+  },
+  {
+    "revision": "c77d26c82e1f5b56cb1478b8cbefaace",
+    "url": "/steam-artwork-gen/static/media/SFProDisplay-ThinItalic.c77d26c8.woff2"
+  },
+  {
+    "revision": "a01866edaa839e84c1253f3b548b2e37",
+    "url": "/steam-artwork-gen/static/media/SFProDisplay-Ultralight.a01866ed.woff2"
+  },
+  {
+    "revision": "f9f8194e56d5423a115936d5b92afd8d",
+    "url": "/steam-artwork-gen/static/media/SFProDisplay-Ultralight.f9f8194e.woff"
+  },
+  {
+    "revision": "2910a3bd7ee2049d2e2815d006c5cfd8",
+    "url": "/steam-artwork-gen/static/media/SFProDisplay-UltralightItalic.2910a3bd.woff2"
+  },
+  {
+    "revision": "cae1c036f76124956aea417a01dd5166",
+    "url": "/steam-artwork-gen/static/media/SFProDisplay-UltralightItalic.cae1c036.woff"
+  },
+  {
+    "revision": "8e175b47e6cc95ff7aa4bf6449f1158d",
+    "url": "/steam-artwork-gen/static/media/SFProText-Bold.8e175b47.woff2"
+  },
+  {
+    "revision": "dcb8aacf0e155556b0784c32f7d95e7b",
+    "url": "/steam-artwork-gen/static/media/SFProText-Bold.dcb8aacf.woff"
+  },
+  {
+    "revision": "1aafd0575f856cfc68bc7a3492d23e24",
+    "url": "/steam-artwork-gen/static/media/SFProText-BoldItalic.1aafd057.woff"
+  },
+  {
+    "revision": "aa43b00efe72e5e1c554c0e56b3aa9ce",
+    "url": "/steam-artwork-gen/static/media/SFProText-BoldItalic.aa43b00e.woff2"
+  },
+  {
+    "revision": "759984921ee8807e7b4930c56eb0de32",
+    "url": "/steam-artwork-gen/static/media/SFProText-Heavy.75998492.woff"
+  },
+  {
+    "revision": "e2a1411345a11d7d65621240838347b3",
+    "url": "/steam-artwork-gen/static/media/SFProText-Heavy.e2a14113.woff2"
+  },
+  {
+    "revision": "1187d77d1c28e9f5da0e649cde66c6f3",
+    "url": "/steam-artwork-gen/static/media/SFProText-HeavyItalic.1187d77d.woff2"
+  },
+  {
+    "revision": "41dfcd0566db0b3189ec299a60b23639",
+    "url": "/steam-artwork-gen/static/media/SFProText-HeavyItalic.41dfcd05.woff"
+  },
+  {
+    "revision": "48e92b33a3289b0a8b1476037532bc55",
+    "url": "/steam-artwork-gen/static/media/SFProText-Light.48e92b33.woff2"
+  },
+  {
+    "revision": "d01ce17d8b0edf8511bd5811721c0a7d",
+    "url": "/steam-artwork-gen/static/media/SFProText-Light.d01ce17d.woff"
+  },
+  {
+    "revision": "6b470ef873a0cb6816e226e01ffd124b",
+    "url": "/steam-artwork-gen/static/media/SFProText-LightItalic.6b470ef8.woff"
+  },
+  {
+    "revision": "d271e6b272b522ad7eda1bbf909e50eb",
+    "url": "/steam-artwork-gen/static/media/SFProText-LightItalic.d271e6b2.woff2"
+  },
+  {
+    "revision": "09be2e2f35fe3d0117ecea6cc463094e",
+    "url": "/steam-artwork-gen/static/media/SFProText-Medium.09be2e2f.woff2"
+  },
+  {
+    "revision": "92ad8f7229b331b2a4a8c0176acf155a",
+    "url": "/steam-artwork-gen/static/media/SFProText-Medium.92ad8f72.woff"
+  },
+  {
+    "revision": "549f50adfb3f940cb04d53f892c9ad96",
+    "url": "/steam-artwork-gen/static/media/SFProText-MediumItalic.549f50ad.woff"
+  },
+  {
+    "revision": "6e632d20ae0208be666ae8b95c747982",
+    "url": "/steam-artwork-gen/static/media/SFProText-MediumItalic.6e632d20.woff2"
+  },
+  {
+    "revision": "06b45761bfabccf92c66efc6e3d6fbec",
+    "url": "/steam-artwork-gen/static/media/SFProText-Regular.06b45761.woff"
+  },
+  {
+    "revision": "c7e2854dfcedf02e21c78e34bd7a7141",
+    "url": "/steam-artwork-gen/static/media/SFProText-Regular.c7e2854d.woff2"
+  },
+  {
+    "revision": "ded7731204ac2cd31794368930486e54",
+    "url": "/steam-artwork-gen/static/media/SFProText-RegularItalic.ded77312.woff"
+  },
+  {
+    "revision": "f49877e06a4e1a0194eb4a16351412d4",
+    "url": "/steam-artwork-gen/static/media/SFProText-RegularItalic.f49877e0.woff2"
+  },
+  {
+    "revision": "211b9d5260f9bb835f5635c688c92e6e",
+    "url": "/steam-artwork-gen/static/media/SFProText-Semibold.211b9d52.woff2"
+  },
+  {
+    "revision": "f9ef29896b27001f1dbaa00acbd5408c",
+    "url": "/steam-artwork-gen/static/media/SFProText-Semibold.f9ef2989.woff"
+  },
+  {
+    "revision": "7867dc7a589081f05a0222c23890e0fc",
+    "url": "/steam-artwork-gen/static/media/SFProText-SemiboldItalic.7867dc7a.woff"
+  },
+  {
+    "revision": "9aa5d618db019034cb9f85d7b178bfbc",
+    "url": "/steam-artwork-gen/static/media/SFProText-SemiboldItalic.9aa5d618.woff2"
+  }
+]);
